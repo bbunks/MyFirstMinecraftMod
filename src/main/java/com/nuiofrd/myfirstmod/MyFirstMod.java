c909d57f615ab2d5006272dd962e857dd3bd0094
nuiofrd <brendenbunker@gmail.com>
2014-10-21 21:17:14 -0700

    mcmod

--- src/main/java/com/nuiofrd/myfirstmod/MyFirstMod.java
+++ src/main/java/com/nuiofrd/myfirstmod/MyFirstMod.java
@@ -1,15 +1,15 @@
-package com.nuiofrd.myfirstminecraftmod;
+package com.nuiofrd.myfirstmod;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = "myFirstMod", name = "My First Mod", version = "1.7.2-1.0")
-public class MyFirstMinecraftMod {
+@Mod(modid = "MyFirstMod", name = "My First Mod", version = "1.7.2-1.0")
+public class MyFirstMod {
 
     @Mod.Instance
-    public static MyFirstMinecraftMod instance;
+    public static MyFirstMod instance;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
